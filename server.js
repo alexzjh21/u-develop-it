232c0d1042c32040c5918e310fe6a24e9b613fe1
alexzjh21 <alexzjh21@yahoo.co.jp>
2022-03-12 15:11:25 -0500

    connect database to server

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //express middleware 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'hagehage',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+  
 
 
 // Default response for any other request (Not Found)
